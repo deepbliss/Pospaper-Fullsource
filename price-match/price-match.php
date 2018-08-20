7f1676d1dd7faf2716da03ce97ad532cdda2e6fc
bhavar <bliss.bhavar@gmail.com>
2018-08-20 18:48:58 +0530

    captcha

--- price-match/price-match.php
+++ price-match/price-match.php
@@ -1,10 +1,4 @@
-<?php
-    $name = $_GET['name'];
-    $price = $_GET['price'];
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>POSPaper Price Match Request</title>
@@ -98,10 +92,6 @@
              document.getElementById('security_code').value = "";
         });
     </script>
-    
-  </head>
-
-<body>
     <div id="wrapper" class="price-match-form content">        
         <form action="<?php echo "https://" . $_SERVER['SERVER_NAME'] . "/price-match/pmcontact.php"; ?>" method="post" name="pricematch" onsubmit="return validateForm();">             
           <h2 class="title">LOW PRICE GUARANTEE</h2>
@@ -289,6 +279,5 @@
  <style type="text/css">
    .content{height: 100vh;}
  </style>
-</body>
-</html>
+
 
