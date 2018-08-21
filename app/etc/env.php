ffc29147287485f8d237c47365ca40a88d7cb322
bhavar <bliss.bhavar@gmail.com>
2018-08-21 14:01:01 +0530

    captcha

--- app/etc/env.php
+++ app/etc/env.php
@@ -31,32 +31,11 @@ return array (
     ),
   ),
   'x-frame-options' => 'SAMEORIGIN',
-  'MAGE_MODE' => 'developer',
-  'session' =>
-array (
-  'save' => 'redis',
-  'redis' =>
+  'MAGE_MODE' => 'default',
+  'session' => 
   array (
-    'host' => '127.0.0.1',
-    'port' => '6379',
-    'password' => '',
-    'timeout' => '2.5',
-    'persistent_identifier' => '',
-    'database' => '2',
-    'compression_threshold' => '2048',
-    'compression_library' => 'gzip',
-    'log_level' => '1',
-    'max_concurrency' => '6',
-    'break_after_frontend' => '5',
-    'break_after_adminhtml' => '30',
-    'first_lifetime' => '600',
-    'bot_first_lifetime' => '60',
-    'bot_lifetime' => '7200',
-    'disable_locking' => '0',
-    'min_lifetime' => '60',
-    'max_lifetime' => '2592000'
-  )
-),
+    'save' => 'files',
+  ),
   'cache_types' => 
   array (
     'config' => 1,
