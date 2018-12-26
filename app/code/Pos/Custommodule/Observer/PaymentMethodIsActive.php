07a7a5f18aa1ab93d11c20d3911ebd9f2a36168c
Alex H <info@designcoil.com>
2018-12-26 04:52:44 -0800

    Purchase Order payment method on the Metro Diner

--- app/code/Pos/Custommodule/Observer/PaymentMethodIsActive.php
+++ app/code/Pos/Custommodule/Observer/PaymentMethodIsActive.php
@@ -10,25 +10,34 @@ class PaymentMethodIsActive implements ObserverInterface
     protected $_groupRepository;
     protected $_state;
     protected $_session;
+    protected $_storeManager;
 
     public function __construct(
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Customer\Model\ResourceModel\GroupRepository $groupRepository,
         \Magento\Framework\App\State $state,
-        \Magento\Backend\Model\Auth\Session $authSession
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         $this->_customerSession = $customerSession;
         $this->_groupRepository = $groupRepository;
         $this->_state = $state;
         $this->_session = $authSession;
+        $this->_storeManager = $storeManager;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $enabled = false;
         $enabledGroups = array('Approved Credit Customer','Metro Diner');
+        $enabledStores = array('metrodiner');
 
-        if($this->_customerSession->isLoggedIn()) {
+        $currentStore = $this->_storeManager->getStore();
+        if(in_array($currentStore->getCode(),$enabledStores)) {
+            $enabled = true;
+        }
+
+        if($this->_customerSession->isLoggedIn() && !$enabled) {
             $customer = $this->_customerSession->getCustomer();
             $customerGroup = $this->_groupRepository->getById($customer->getGroupId());
             $enabled = in_array($customerGroup->getCode(),$enabledGroups);
