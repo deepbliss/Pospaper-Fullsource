7fb8a22dd38608a3569dc54a5d4cfef97ad3542b
Alex H <info@designcoil.com>
2019-01-05 07:11:05 -0800

    Replace the three occurrences of "Metro Diner"

--- app/code/Pos/Custommodule/Helper/Data.php
+++ app/code/Pos/Custommodule/Helper/Data.php
@@ -5,12 +5,22 @@
 namespace Pos\Custommodule\Helper;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
+    protected $storeManager;
 
 	/**
      * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
-	public function __construct(\Magento\Framework\App\Helper\Context $context
+	public function __construct(
+	    \Magento\Framework\App\Helper\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
 	) {
 		parent::__construct($context);
+		$this->storeManager = $storeManager;
+	}
+
+	public function getStore()
+    {
+        return $this->storeManager->getStore();
 	}
 }
