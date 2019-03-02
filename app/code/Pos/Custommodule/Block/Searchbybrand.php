88609af4a5abc5bdc8795873ad03b7d6a4813684
Alex H <info@designcoil.com>
2019-03-02 09:02:13 -0800

    Remove Paper Roll Search Tool from Restaurant Crayons category

--- app/code/Pos/Custommodule/Block/Searchbybrand.php
+++ app/code/Pos/Custommodule/Block/Searchbybrand.php
@@ -5,20 +5,35 @@ namespace Pos\Custommodule\Block;
 class Searchbybrand extends \Magento\Framework\View\Element\Template
 {
     protected $manufacturerCollectionFactory;
+    protected $registry;
 
     public function __construct(
         \Pos\Manufacturer\Model\ResourceModel\Manufacturer\CollectionFactory $manufacturerCollectionFactory,
+        \Magento\Framework\Registry $registry,
         \Magento\Framework\View\Element\Template\Context $context,   
         array $data = []
     ){        
         parent::__construct($context,$data);
         $this->manufacturerCollectionFactory = $manufacturerCollectionFactory;
+        $this->registry = $registry;
     }
 
-    public function getAllBrand(){
-        //$objectManager =  \Magento\Framework\App\ObjectManager::getInstance();
-        //$getOptionValue = $objectManager->create('Pos\Manufacturer\Model\Manufacturer')->getCollection();
+    public function getAllBrand()
+    {
         $brands = $this->manufacturerCollectionFactory->create();
         return $brands;
-    }  
+    }
+
+    public function canShowPaperRollSearchTool()
+    {
+        $category = $this->registry->registry('current_category');
+        if(!$category) {
+            return true;
+        }
+        if($category->getData('custom_attribute1')) {
+            return true;
+        }
+
+        return false;
+    }
 }
