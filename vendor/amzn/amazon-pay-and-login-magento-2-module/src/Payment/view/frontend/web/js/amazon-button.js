a2275cb816174a36c50d76b0b65c7e0b9045f44c
Alex H <info@designcoil.com>
2019-04-02 15:03:59 -0700

    https://trello.com/c/oVwHB3Qe/194-amazon-button-missing

--- vendor/amzn/amazon-pay-and-login-magento-2-module/src/Payment/view/frontend/web/js/amazon-button.js
+++ vendor/amzn/amazon-pay-and-login-magento-2-module/src/Payment/view/frontend/web/js/amazon-button.js
@@ -44,7 +44,8 @@ define([
             _this = this;
             $button = this.element;
             this._verifyAmazonConfig();
-            _this._renderAmazonButton();
+            window.setTimeout( _this._renderAmazonButton, 1000 );
+
         },
 
         /**
