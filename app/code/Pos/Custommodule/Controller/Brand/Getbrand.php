9ae4d209499295328b8e66a929826d9688f4eae6
Alex H <info@designcoil.com>
2019-04-05 06:59:22 -0700

    Server error (5xx)

--- app/code/Pos/Custommodule/Controller/Brand/Getbrand.php
+++ app/code/Pos/Custommodule/Controller/Brand/Getbrand.php
@@ -7,32 +7,44 @@ use Magento\Framework\App\Action\Context;
 class Getbrand extends \Magento\Framework\App\Action\Action
 {
     protected $_resultPageFactory;
+    protected $_resultForwardFactory;
  
-    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
-    {
+    public function __construct(
+        Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory
+    ) {
         $this->_resultPageFactory = $resultPageFactory;
+        $this->_resultForwardFactory = $resultForwardFactory;
         parent::__construct($context);
     }
  
     public function execute()
     {
-        $manufacturer = $_POST['manufacturer'];   
+        if(isset($_POST['manufacturer'])) {
+            $html = '';
+            $manufacturer = $_POST['manufacturer'];
 
-        $objectManager =  \Magento\Framework\App\ObjectManager::getInstance();        
-        
-        $getOptionValue = $objectManager->create('Pos\Manufacturer\Model\Manufacturer')->getCollection();
-        $getOptionValue->addFieldToFilter('manufacturer', $manufacturer);
-        $mod = [];
-        $html = '';
-        foreach($getOptionValue as $value){
-            $mod[$value->getData('model')] = $value->getData('model');
-        }
-        asort($mod);
+            $objectManager =  \Magento\Framework\App\ObjectManager::getInstance();
 
-        $html .= '<option value="-1" >Select a Model</option>';
-        foreach ($mod as $value){
-            $html .= '<option value="'.$value.'" >'.$value.'</option>';
+            $getOptionValue = $objectManager->create('Pos\Manufacturer\Model\Manufacturer')->getCollection();
+            $getOptionValue->addFieldToFilter('manufacturer', $manufacturer);
+            $mod = [];
+
+            foreach($getOptionValue as $value){
+                $mod[$value->getData('model')] = $value->getData('model');
+            }
+            asort($mod);
+
+            $html .= '<option value="-1" >Select a Model</option>';
+            foreach ($mod as $value){
+                $html .= '<option value="'.$value.'" >'.$value.'</option>';
+            }
+            echo $html;
+        } else {
+            $resultForward = $this->_resultForwardFactory->create();
+            $resultForward->forward('defaultIndex');
+            return $resultForward;
         }
-        echo $html;
     }
 }
